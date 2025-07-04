23fe2a352cc112c41fcff41367828f23e7df6842
anish <akumar74433@gmail.com>
2025-07-04 17:53:12 +0530

    First

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div className="text-2xl  text-rose-500">
+    Hello Anish 
+    </div>
+  );
+}
